38e83af3e080bf92e2fe5ef4cc22b1c548856795
Remix Workspace <>
2022-10-30 23:12:01 +0100

    successful getBalanceOf IERC20 call

--- EtherWallet/EtherWallet.sol
+++ EtherWallet/EtherWallet.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "./IERC20custom.sol";
+
 contract EtherWallet {
     address payable public owner;
 
@@ -19,7 +21,10 @@ contract EtherWallet {
         return address(this).balance;
     }
 
-    function getBalanceMod(address erc20) external view returns (uint){
-        return address(erc20).balanceOf(address(this));
+    // find balance of any erc20 token associated with this address
+    function balanceOfThisToken(address _erc20) external view returns (uint){
+        ERC20Standard erc20instance = ERC20Standard(_erc20);
+        return erc20instance.balanceOf(address(this));
     }
 }
+
---
+++ EtherWallet/IERC20custom.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+interface ERC20Standard {
+    function balanceOf(address account) external view returns (uint);
+}
