2559c822b5928490d1ef2183c61ae94bae10582f
0xheartcode <0xheartcode@gmail.com>
2022-12-15 01:46:44 +0100

    cleaned up learning archive.

--- EtherWallet/IERC20custom.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.13;
-
-interface ERC20Standard {
-    function balanceOf(address account) external view returns (uint);
-}
---
+++ SolidityByExample/2_Hacks/15_ReadOnlyReEntrancy.sol
@@ -0,0 +1,2 @@
+// 
+https://www.youtube.com/watch?v=0fgGTRlsDxI&list=PLO5VPQH6OWdWsCgXJT9UuzgbC8SPvTRi5&index=16
